8a30b6a5ae441611f7c0a3fa180dda82f7ee8f2a
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-02-23 20:38:38 +0200

    Post processing

---
+++ app.py
@@ -0,0 +1,45 @@
+from http.server import BaseHTTPRequestHandler
+import socketserver
+from email.parser import BytesParser
+from email.policy import default
+
+PORT = 8000
+
+class ImageFilerServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open('templates/index.html', 'rb') as file:
+                self.wfile.write(file.read())
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        file_content = self.rfile.read(content_length)
+        boundary = self.headers['Content-Type'].split('boundary=')[1]
+        parts = file_content.split(b'--' + boundary.encode())
+
+        parts_dict = dict()
+
+        for part in parts:
+            if part.strip():
+                msg = BytesParser(policy=default).parsebytes(part.strip())
+                name = msg.get_param('name', header='content-disposition')
+                # filename = msg.get_param('filename')
+                filename = msg.get_filename()
+                payload = msg.get_payload(decode=True)
+
+                parts_dict[name] = (filename, payload)
+            # if part.strip():
+        
+        filename, payload = parts_dict['image']
+        with open(filename, 'wb') as file:
+            file.write(payload)
+
+        filter = parts_dict['filter'][1]
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(("", PORT), ImageFilerServer) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
